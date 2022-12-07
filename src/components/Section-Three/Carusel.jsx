f87ea02b986f3fed6404fb4244bf9cded6c28009
Daniel <thepretki@gmail.com>
2022-12-07 03:20:29 +0100

    added new project to carusel

--- src/components/Section-Three/Carusel.jsx
+++ src/components/Section-Three/Carusel.jsx
@@ -2,6 +2,7 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import slide_1 from "../../img/1.png";
 import slide_2 from "../../img/2.png";
 import slide_3 from "../../img/3.png";
+import slide_4 from "../../img/4.png";
 import "@splidejs/splide/css";
 const Carusel = () => {
   return (
@@ -117,6 +118,41 @@ const Carusel = () => {
               </a>
               <a
                 href="https://github.com/Endward01/Sudoku-Solver"
+                className="  text-white font-medium px-8 py-3 rounded-md transition-all duration-[50ms] md:duration-500 bg-gradient-to-r from-tertiary to-[#7e57c2] bg-size-200 bg-pos-0 active:bg-pos-100 md:hover:bg-pos-100"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div className=" w-full h-full rounded-xl bg-tertiary">
+          <div
+            className="rounded-xl bg-cover md:bg-center h-2/3 md:h-4/5"
+            style={{ backgroundImage: `url(${slide_4})` }}
+          ></div>
+          <div className="h-1/3 md:h-1/5 py-4 px-8 shadow-tertiary shadow-lg md:flex md:justify-between md:items-center">
+            <div className="text-center md:text-left h-1/2 md:h-[unset]">
+              <h3 className="text-2xl pb-4 font-bold">Calculator</h3>
+              <p className=" text-base py-2 leading-8 text-gray-400">
+                {" "}
+                Calculator created using reactJs library.
+              </p>
+            </div>
+            <div className=" flex flex-row gap-8 justify-center items-center h-1/2  md:h-[unset]">
+              <a
+                href="https://poetic-crumble-dffc02.netlify.app/"
+                className="  text-white font-medium px-8 py-3 rounded-md transition-all duration-[50ms] md:duration-500 bg-gradient-to-r from-tertiary to-[#7e57c2] bg-size-200 bg-pos-0 active:bg-pos-100 md:hover:bg-pos-100"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Preview
+              </a>
+              <a
+                href="https://github.com/Endward01/calculator-with-reactjs"
                 className="  text-white font-medium px-8 py-3 rounded-md transition-all duration-[50ms] md:duration-500 bg-gradient-to-r from-tertiary to-[#7e57c2] bg-size-200 bg-pos-0 active:bg-pos-100 md:hover:bg-pos-100"
                 target="_blank"
                 rel="noreferrer"
