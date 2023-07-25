85d3b954b1839f8512876dd75331f45ab8dff056
Daniel Pretki <thepretki@gmail.com>
2023-07-25 02:43:11 +0200

    removed resume from website for privacy

--- src/components/Section-One/Navbar.jsx
+++ src/components/Section-One/Navbar.jsx
@@ -1,18 +1,18 @@
-import resume from "../../docs/resume.pdf";
+// import resume from "../../docs/resume.pdf";
 
 const Navbar = () => {
   return (
     <nav className="py-5 md:mb-12 flex justify-between">
       <h1 className="text-xl py-2 font-bold">Web Developer</h1>
 
-      <a
+      {/* <a
         href={resume}
         className="ease-in-out duration-100 btn font-bold px-8 py-3 rounded"
         target="_blank"
         rel="noreferrer"
       >
         Resume
-      </a>
+      </a> */}
     </nav>
   );
 };
